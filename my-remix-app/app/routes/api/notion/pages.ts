eeaf7ea0e6804caa7e422db3eeb914bdf31a6784
TAKE Takashi <nakamizo.takashi@gmail.com>
2022-12-22 16:26:32 +0000

    画像URLのリストが出力されるところまで

--- my-remix-app/app/routes/api/notion/pages.ts
+++ my-remix-app/app/routes/api/notion/pages.ts
@@ -7,11 +7,19 @@ export async function loader({ context }: LoaderArgs) {
   });
   const databaseId: string = context.NOTION_API_API_TEST_DB as string;
 
-  console.log("param1", databaseId);
-
   const pages: string[] = await getPageIdFromDatabase(notion, databaseId);
 
-  return new Response(JSON.stringify(pages), {
+  // すべてのページから画像URLを取得し配列に格納する
+  let images: string[] = [];
+  await Promise.all(
+    pages.map(async (page: string) => {
+      // 取得した画像URLのリストを配列に格納
+      const urls = await getImagesFromPage(notion, page);
+      images = images.concat(urls);
+    })
+  );
+
+  return new Response(JSON.stringify(images), {
     headers: {
       "Content-Type": "application/json",
     },
@@ -19,10 +27,10 @@ export async function loader({ context }: LoaderArgs) {
 }
 
 /**
- * データベースIDからDB内のページIDリストを返す。
  *
- * @param databaseId
- * @returns ページのIDリスト
+ * @param notion NotionClinet
+ * @param databaseId データベースのID
+ * @returns 指定データベース内のページIDのリスト
  */
 async function getPageIdFromDatabase(
   notion: NotionClient,
@@ -39,4 +47,35 @@ async function getPageIdFromDatabase(
   });
 
   return pages;
+}
+
+/**
+ *
+ * @param notion NotionClient
+ * @param pageId ページID
+ * @returns 画像URL文字列が格納された配列
+ */
+async function getImagesFromPage(
+  notion: NotionClient,
+  pageId: string
+): Promise<string[]> {
+  // ページIDからブロックリストを取得する
+  const blocks = await notion.blocks.children.list({
+    block_id: pageId,
+  });
+
+  // ブロックリスト内の画像URLをすべて配列に格納
+  const urlList: string[] = [];
+
+  await Promise.all(
+    blocks.results.map(async (result: any) => {
+      // blockオブジェクト内にimageプロパティがあれば画像のURLを取得
+      if ("image" in result) {
+        const image = result.image.file.url;
+        urlList.push(image);
+      }
+    })
+  );
+
+  return urlList;
 }
